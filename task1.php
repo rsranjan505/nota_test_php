513e6b53b8a480859285f6d769bc66d458ee9e1c
rsranjan505 <94833475+rsranjan505@users.noreply.github.com>
2023-11-04 15:45:29 +0530

    Update task1.php

--- task1.php
+++ task1.php
@@ -1,2 +1,14 @@
-<?
-// Your code is here:
+<?php
+
+$dir = "D:/xampp/htdocs/nomediatest";
+$files = scandir($dir);
+
+sort($files);
+
+foreach($files as $file){
+    if(is_file($dir . '/' .$file) && preg_match('/^[a-zA-Z0-9]+\.(ixt)$/',$file)){
+        echo $file . "</br>";
+    }
+}
+
+?>
