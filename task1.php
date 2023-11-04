3db4c50f7f2f5245b622c0ac517c884dd901d307
rsranjan505 <94833475+rsranjan505@users.noreply.github.com>
2023-11-04 11:37:29 +0530

    Update task1.php

--- task1.php
+++ task1.php
@@ -1,2 +1,14 @@
+<?php
+$dir = "D:/xampp/htdocs/nomediatest";
+$files = scandir($dir);
+
+sort($files);
+
+foreach($files as $file){
+    if(is_file($dir . '/' .$file) && preg_match('/^[a-zA-Z0-9]+\.(ixt)$/',$file)){
+        echo $file . "</br>";
+    }
+}
 <?
-// Your code is here:
+
+
